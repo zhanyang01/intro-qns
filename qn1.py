35e0291117d0f21d63ee7501888b32df7f05a4ae
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-20 15:06:13 +0800

    edit

--- qn1.py
+++ qn1.py
@@ -1,7 +1,15 @@
-chicken = input("Amount of chicken in kg per diner:")
-rice = input("Amount of rice in kg per diner:")
-chilli = input("Amount of chilli in kg per diner:")
+while True:
+    chicken = input("Amount of chicken in kg per diner:")
+    if chicken < 1:
+        break
+while True:
+    rice = input("Amount of rice in kg per diner:")
+    if rice < 1:
+        break
+while True:
+    chilli = input("Amount of chilli in kg per diner:")
+    if chilli < 1:
+        break
 
 Diners = input("Number of diner:")
 
-total = float(chicken + rice + chilli)
