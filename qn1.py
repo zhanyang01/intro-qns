7a067fde538049ca85755e8ea10fb526c8475b67
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-20 14:52:13 +0800

    put it in python file

---
+++ qn1.py
@@ -0,0 +1,7 @@
+chicken = input("Amount of chicken in kg per diner:")
+rice = input("Amount of rice in kg per diner:")
+chilli = input("Amount of chilli in kg per diner:")
+
+Diners = input("Number of diner:")
+
+total = float(chicken + rice + chilli)
