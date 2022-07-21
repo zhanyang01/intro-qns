876c57081c42950c5f19f468951ec55dabbfb297
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-21 15:17:03 +0800

    redo

--- qn5.py
+++ qn5.py
@@ -1,1 +1,14 @@
-num1,num2,num3 = input("Give me 3 integers:").split
+num1,num2,num3 = input("Give me 3 integers:").split()
+num_1 = int(num1)
+num_2 = int(num2)
+num_3 = int(num3)
+
+largest = num_3
+
+if num_1 > num_2 and num_1 > num_3:
+    largest = num_1
+elif num_2 > num_1 and num_2 > num_3:
+    largest = num_2
+
+print(largest)
+
