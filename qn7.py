71c6dddf2e7583e3da5fc1bfbf40d531c2e89760
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-21 22:38:20 +0800

    ..

--- qn7.py
+++ qn7.py
@@ -1,4 +1,7 @@
-positive_int = int(input("number:"))
+while True:
+    positive_int = int(input("number:"))
+    if positive_int > 0:
+        break
 
 for i in range(10):
     result = (i + 1) * positive_int
