89c20b54b99c69ae78614001980c670f6da7a2a5
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-21 22:21:16 +0800

    debug done

--- qn6.py
+++ qn6.py
@@ -4,12 +4,14 @@ temp = float(input("temperature:"))
 if temperature_type == "C":
     if temp > 0 and temp < 100:
         Final = (temp * 1.8) + 32
+        print(Final)
     else:
         print("Error as water is no longer in liquid state")
 
 if temperature_type == "F":
     if temp > 32 and temp < 212:
         Final = (temp - 32)/1.8
+        print(Final)
     else:
         print("Error as water is no longer in liquid state")    
 
