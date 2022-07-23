b95b5f48c834f94e1afb3e683be10f6ce0379940
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-23 22:15:21 +0800

    n

---
+++ qn10.py
@@ -0,0 +1,14 @@
+while True:
+    positive_integer = int(input("Positive integer:"))
+    if positive_integer > 0
+        break
+
+i = 1
+for i in range(8):
+    if positive_integer >= 10:
+        positive_integer/10
+        i += 1
+    else:
+        print(positive_integer)
+
+        
