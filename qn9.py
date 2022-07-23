be928a9faeaa8fabdb46603c528b2ffca5434305
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-23 16:17:43 +0800

    pre debug

---
+++ qn9.py
@@ -0,0 +1,24 @@
+while True:
+    positive_number = int(input("Positive number :"))
+    if positive_number > 0 and positive_number < 100:
+        break
+
+squ = False
+i = 1
+
+while i <= 10:
+    square = i**2
+    if positive_number == square
+        squ = True
+        break
+    else:
+        squ = False
+    i += 1
+
+if squ = True:
+    print("Perfect square")
+else
+    print("Not perfect square")
+
+    
+
