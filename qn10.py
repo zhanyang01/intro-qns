6ef38264d40217f314ba6a6481dd8efccdb70bdf
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-23 22:19:44 +0800

    rebug

--- qn10.py
+++ qn10.py
@@ -3,12 +3,13 @@ while True:
     if positive_integer > 0:
         break
 
+positive = positive_integer
 i = 1
 for i in range(8):
     if positive_integer >= 10:
-        positive_integer/10
+        positive = positive_integer/10
         i += 1
     else:
-        print(positive_integer)
+        print(positive)
 
         
