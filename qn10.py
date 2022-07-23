fa453399315ccc8758ea0923164841f6a09268bf
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-23 22:25:49 +0800

    y

--- qn10.py
+++ qn10.py
@@ -7,7 +7,7 @@ positive = positive_integer
 for i in range(8 ,1,-1):
     if positive >= 10:
         positive = positive_integer/10
-        
+        positive_integer = positive   
     else:
         break
 
